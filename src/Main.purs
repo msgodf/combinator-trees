6d3807e902d75ed09a705ac997fd89ef8fad41b7
Mark Godfrey <msgodf@gmail.com>
2017-01-20 09:02:20 +0000

    Remove unused structure and imports.

--- src/Main.purs
+++ src/Main.purs
@@ -4,29 +4,18 @@ module Main ( main
 import Parsing as P
 import Render (drawTree, parseTreeToSimpleTree,treeData)
 import D3.Base (D3)
-import D3.Selection (Selection)
 
-import Prelude (class Show, Unit, Void, bind, pure, show, ($), (<>))
+import Prelude (Unit, bind, show, ($), (<>))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Function.Uncurried (Fn1, runFn1)
 import Data.Either (Either(..))
-import Data.Foreign.Class (class IsForeign, readProp, write)
+import Data.Foreign.Class (write)
 
 -- Helper function that calls console.log with anything
 foreign import logAnythingImpl :: forall a e. (Fn1 a (Eff (console :: CONSOLE | e) Unit))
 logAnything :: forall a e. a -> (Eff (console :: CONSOLE | e) Unit)
 logAnything = (runFn1 logAnythingImpl)
-
-data NodeData a = NodeData {data':: a}
-
-instance nodeDataShow :: (Show a) => Show (NodeData a) where
-  show (NodeData {data':data'}) = "data: " <> (show data')
-
-instance nodeDataIsForeign :: (IsForeign a) => IsForeign (NodeData a) where
-  read x = do
-    data' <- readProp "data" x
-    pure $ NodeData {data': data'}
 
 drawTreeForExpression :: forall e. String -> Eff (d3 :: D3, console :: CONSOLE | e) Unit
 drawTreeForExpression x = case (P.parseTreeExpression x) of
@@ -36,5 +25,5 @@ drawTreeForExpression x = case (P.parseTreeExpression x) of
                 log "Parsed"
 
 
-main :: forall e. Eff (d3 :: D3, console :: CONSOLE | e) (Selection Void)
+main :: forall a e. Eff (d3 :: D3, console :: CONSOLE | e) a
 main = drawTree treeData
